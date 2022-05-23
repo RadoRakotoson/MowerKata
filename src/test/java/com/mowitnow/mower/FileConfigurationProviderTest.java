cf49f07ccdc3ebcb06460c042697f96818762825
Rado Rakotoson <rado.rakotoson@shodo.io>
2022-05-23 21:28:18 +0200

    Must get number of mowers

--- src/test/java/com/mowitnow/mower/FileConfigurationProviderTest.java
+++ src/test/java/com/mowitnow/mower/FileConfigurationProviderTest.java
@@ -1,8 +1,12 @@
-package com.mowitnow.mower.infrastructure;
+package com.mowitnow.mower;
 
 import com.mowitnow.mower.domain.Dimension;
 import com.mowitnow.mower.domain.Garden;
+import com.mowitnow.mower.domain.Mower;
+import com.mowitnow.mower.infrastructure.FileConfigurationProvider;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +18,12 @@ public class FileConfigurationProviderTest {
         Garden garden = configurationProvider.getGardenInformation();
 
         assertThat(garden).isEqualTo(new Garden(new Dimension(5, 5)));
+    }
+
+    @Test
+    void mowers_should_be_parsed_from_configuration_file() {
+        List<Mower> mowers = configurationProvider.getAllAvailableMowers();
+
+        assertThat(mowers.size()).isEqualTo(2);
     }
 }
--- src/test/java/com/mowitnow/mower/infrastructure/FileConfigurationProvider.java
+++ src/test/java/com/mowitnow/mower/infrastructure/FileConfigurationProvider.java
@@ -1,38 +1,85 @@
 package com.mowitnow.mower.infrastructure;
 
-import com.mowitnow.mower.domain.Dimension;
-import com.mowitnow.mower.domain.Garden;
-import com.mowitnow.mower.domain.Instruction;
-import com.mowitnow.mower.domain.Mower;
+import com.mowitnow.mower.domain.*;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.mowitnow.mower.domain.Direction.*;
+import static java.lang.Integer.parseInt;
 import static java.nio.file.Files.readString;
 import static java.util.Objects.requireNonNull;
 
 public class FileConfigurationProvider implements ConfigurationProvider {
 
+    private final Garden garden;
+    private final List<Mower> mowers;
+
     public FileConfigurationProvider(String configurationFile) throws URISyntaxException, IOException {
         URI fileLocation = requireNonNull(getClass().getClassLoader().getResource(configurationFile)).toURI();
         String[] configurations = readString(Paths.get(fileLocation)).split("\n");
+
+        garden = getGardenInformationFromConfiguration(configurations[0]);
+        mowers = getAllAvailableMowersFromConfiguration(configurations);
     }
 
     @Override
     public Garden getGardenInformation() {
-        return new Garden(new Dimension(5, 5));
+        return garden;
     }
 
     @Override
     public List<Mower> getAllAvailableMowers() {
-        return null;
+        return mowers;
     }
 
     @Override
     public List<Instruction> getAllInstructionFor(int mowerIndex) {
-        return null;
+        return new ArrayList<>();
+    }
+
+    private Garden getGardenInformationFromConfiguration(String gardenConfiguration) {
+        int limitX = parseInt(gardenConfiguration.split(" ")[0]);
+        int limitY = parseInt(gardenConfiguration.split(" ")[1]);
+        Dimension gardenDimension = new Dimension(limitX, limitY);
+        return new Garden(gardenDimension);
+    }
+
+    private List<Mower> getAllAvailableMowersFromConfiguration(String[] configurations) {
+        List<Mower> mowers = new ArrayList<>();
+        for (int i = 1; i < configurations.length; i += 2) {
+            mowers.add(new Mower(toPosition(configurations[i]), toDirection(configurations[i])));
+        }
+
+        return mowers;
+    }
+
+    private Position toPosition(String positionRaw) {
+        String[] originArraySpitted = positionRaw.split(" ");
+        int x = parseInt(originArraySpitted[0]);
+        int y = parseInt(originArraySpitted[1]);
+        return new Position(x, y);
+    }
+
+    private Direction toDirection(String directionRaw) {
+        String[] originArraySpitted = directionRaw.split(" ");
+        switch (originArraySpitted[2]) {
+            case "N" -> {
+                return NORTH;
+            }
+            case "E" -> {
+                return EAST;
+            }
+            case "S" -> {
+                return SOUTH;
+            }
+            default -> {
+                return WEST;
+            }
+        }
     }
 }
